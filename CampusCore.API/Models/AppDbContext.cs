71c0bf6e47b517c2ed1297f67eb4278fd469556c
Shaireen Alexis Moncada <shaireenalexismoncada@gmail.com>
2023-10-02 18:57:48 +0800

    initial commit

---
+++ CampusCore.API/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampusCore.API.Models
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext (DbContextOptions options) : base(options) { 
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .Property(u => u.Status)
+                .HasDefaultValue(UserStatus.Inactive) // Set the default value
+                .HasConversion<string>(); // Convert enum to string for the database
+
+            // Add other configurations for your ApplicationUser model here
+        }
+    }
+}
