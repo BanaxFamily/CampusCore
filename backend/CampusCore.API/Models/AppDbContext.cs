0698e78171b3f96a87ebec225ebd48b3dbd54473
Shaireen Alexis Moncada <shaireenalexismoncada@gmail.com>
2023-11-01 22:29:38 +0800

    student group update

--- backend/CampusCore.API/Models/AppDbContext.cs
+++ backend/CampusCore.API/Models/AppDbContext.cs
@@ -1,28 +1,63 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace CampusCore.API.Models
 {
     public class AppDbContext : IdentityDbContext
     {
-        public AppDbContext (DbContextOptions options) : base(options) { 
-        
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
         }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<Course> Courses { get; set; }  
-        public DbSet<Deliverable> Deliverables { get; set; }
+        public DbSet<UserLog> UserLogs { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<OfferedCourse> OfferedCourses { get; set; }
+        public DbSet<CourseEnrollment> CourseEnrollments { get; set; }
+        public DbSet<StudentGroup> StudentGroups { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
+            // Configure the relationship between OfferedCourse and Course
+            builder.Entity<OfferedCourse>()
+                .HasOne(oc => oc.Course)
+                .WithMany()
+                .HasForeignKey(oc => oc.CourseId).OnDelete(DeleteBehavior.NoAction);
+
+            // Configure the relationship between OfferedCourse and FacultyMember (and Reviewer)
+            builder.Entity<OfferedCourse>()
+                .HasOne(oc => oc.FacultyAssigned)
+                .WithMany()
+                .HasForeignKey(oc => oc.FacultyId).OnDelete(DeleteBehavior.NoAction);
+
+            // Configure the many-to-many relationship between OfferedCourse and User through CourseEnrollment
+            builder.Entity<CourseEnrollment>()
+            .HasKey(ce => new { ce.OfferedCourseId, ce.StudentId }); ;
+
+            builder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.Student)
+                .WithMany()
+                .HasForeignKey(ce => ce.StudentId);
+
+            builder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.OfferedCourse)
+                .WithMany()
+                .HasForeignKey(ce => ce.OfferedCourseId);
+
+            builder.Entity<StudentGroup>()
+                .HasMany(sg => sg.Members)
+                .WithOne(u => u.StudentGroup)
+                .HasForeignKey(u => u.StudentGroupId); // Explicitly specify the foreign key for Members
+            builder.Entity<StudentGroup>()
+                .HasOne(sg => sg.Adviser)
+                .WithMany()
+                .HasForeignKey(sg => sg.AdviserId);
+
+
             base.OnModelCreating(builder);
-
-            //builder.Entity<User>()
-            //    .Property(u => u.Status)
-            //    .HasDefaultValue(UserStatus.Inactive) // Set the default value
-            //    .HasConversion<string>(); // Convert enum to string for the database
-
-            //// Add other configurations for your ApplicationUser model here
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ backend/CampusCore.API/Models/UserLog.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CampusCore.API.Models
+{
+    public class UserLog
+    {
+        public int Id { get; set; }
+        public DateTime Log { get; set; }
+
+        //
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
