8ddaaaa40f861ead4209d324f91328b291ca71ca
Alixander Lawan <alixander.lawan@gmail.com>
2023-11-04 16:51:24 +0800

    Added ViewModels

--- backend/CampusCore.Shared/DeliverableViewModels.cs
+++ backend/CampusCore.Shared/DeliverableViewModels.cs
@@ -8,9 +8,33 @@ namespace CampusCore.Shared
 {
     public class DeliverableAddViewModel
     {
-        //public string Name { get; set; }
-        //public string Description { get; set; }
-        //public string Instruction { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+
+        //public int Course_Id { get; set; } // to be added: connection to other database. in a comment for now; to be updated soon
+    }
+    public class DeliverableListViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+
+        //public int Course_Id { get; set; } // to be added: connection to other database. in a comment for now; to be updated soon
+    }
+    public class DeliverableDeleteViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
+
+        //public int Course_Id { get; set; } // to be added: connection to other database. in a comment for now; to be updated soon
+    }
+    public class DeliverableUpdateViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instruction { get; set; }
 
         //public int Course_Id { get; set; } // to be added: connection to other database. in a comment for now; to be updated soon
     }
---
+++ backend/CampusCore.Shared/SubmissionIssueViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusCore.Shared
+{
+    internal class SubmissionIssueAddViewModel
+    {
+        public int targetIssueId { get; set; }
+        //public int submissionId { get; set;} // to add foreign key from submissionTable ?
+        //public int versionId { get; set;} // lacking foreign key from VersionTable ?
+        //public int issueId { get; set; } // lacking foreign key from IssueTable
+    }
+    internal class SubmissionIssueListViewModel
+    {
+        public int targetIssueId { get; set; }
+        //public int submissionId { get; set;} // to add foreign key from submissionTable ?
+        //public int versionId { get; set;} // lacking foreign key from VersionTable ?
+        //public int issueId { get; set; } // lacking foreign key from IssueTable
+    }
+    internal class SubmissionIssueDeleteViewModel
+    {
+        public int targetIssueId { get; set; }
+        //public int submissionId { get; set;} // to add foreign key from submissionTable ?
+        //public int versionId { get; set;} // lacking foreign key from VersionTable ?
+        //public int issueId { get; set; } // lacking foreign key from IssueTable
+    }
+    internal class SubmissionIssueUpdateViewModel
+    {
+        public int targetIssueId { get; set; }
+        //public int submissionId { get; set;} // to add foreign key from submissionTable ?
+        //public int versionId { get; set;} // lacking foreign key from VersionTable ?
+        //public int issueId { get; set; } // lacking foreign key from IssueTable
+    }
+}
