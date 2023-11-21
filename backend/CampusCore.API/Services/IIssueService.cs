95e7e89328c5bfdaa8cbcf083e678456f23d0e2b
Shaireen Moncada <114043416+shaiweeeen@users.noreply.github.com>
2023-11-21 10:20:58 +0800

    Revert "issue adjustment"

--- backend/CampusCore.API/Services/IIssueService.cs
+++ backend/CampusCore.API/Services/IIssueService.cs
@@ -20,7 +20,6 @@ namespace CampusCore.API.Services
         Task<ResponseManager> IssueGetByIdAsync(IntIdViewModel model);
         Task<ResponseManager> DeleteIssueAsync(IntIdViewModel model);
         Task<ResponseManager> UpdateIssueAsync(IssueUpdateViewModel model);
-        Task<ResponseManager> SearchIssueAsync(StringSearchViewModel model); 
 
 
     }
@@ -97,37 +96,6 @@ namespace CampusCore.API.Services
 
 
         }
-        public async Task<ResponseManager> SearchIssueAsync(StringSearchViewModel model)
-        {
-            string searchKey = model.SearchKey;
-
-            try
-            {
-
-                var searchResults = await _context.Issues
-                    .Where(oc => EF.Functions.Like(oc.Name, $"%{searchKey}%"))
-                    .ToListAsync();
-
-
-
-                return new DataResponseManager
-                {
-                    IsSuccess = true,
-                    Message = "Searched issues retrieved successfully",
-                    Data = searchResults
-                };
-            }
-            catch (Exception ex)
-            {
-                return new ErrorResponseManager
-                {
-                    IsSuccess = false,
-                    Message = "An error occurred while fetching searched issues",
-                    Errors = new List<string> { ex.Message }
-                };
-            }
-        }
-
 
         public async Task<ResponseManager> SearchIssueAsync(StringSearchViewModel model)
         {
@@ -417,6 +385,9 @@ namespace CampusCore.API.Services
             }
         }
 
+
+
+        
 
 
         public async Task<ResponseManager> DeleteIssueAsync(IntIdViewModel model)
