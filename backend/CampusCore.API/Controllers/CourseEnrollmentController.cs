9387168e95d37f3b90cca16a0f29c28e0e6207e8
Sandatang <mushi.elite@gmail.com>
2023-11-22 15:43:16 +0800

    Finished courseloads

--- backend/CampusCore.API/Controllers/CourseEnrollmentController.cs
+++ backend/CampusCore.API/Controllers/CourseEnrollmentController.cs
@@ -69,7 +69,7 @@ namespace CampusCore.API.Controllers
 
 
         [HttpDelete("delete")]
-        [Authorize(Roles = "Admin,Dean")]
+        //[Authorize(Roles = "Admin,Dean")]
         public async Task<IActionResult> DeleteAsync(IntIdViewModel model)
         {
             if (ModelState.IsValid)
