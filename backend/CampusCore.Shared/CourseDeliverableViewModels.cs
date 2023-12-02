7685e3e491a096b552ae7ce41bdd72ca82e37347
Shaireen Alexis Moncada <shaireenalexismoncada@gmail.com>
2023-12-02 23:29:48 +0800

    course-deliverable changes

--- backend/CampusCore.Shared/CourseDeliverableViewModels.cs
+++ backend/CampusCore.Shared/CourseDeliverableViewModels.cs
@@ -10,18 +10,6 @@ namespace CampusCore.Shared
     {
         public int CourseId { get; set; }
         public int DeliverableId { get; set; }
-        public DateTime? DeliverableDeadline { get; set; }
     }
-    public class CourseDeliverableListViewModel
-    {
-        public int Id { get; set; }
-        public int CourseId { get; set; }
-        public int DeliverableId { get; set; } 
-        public DateTime? DeliverableDeadline { get; set; }
-    }
-    public class CourseDeliverableUpdateViewModel
-    {
-        
-        public DateTime DeliverableDeadline { get; set; }
-    }
+    
 }
