e61dc1fd4d7a2c436fb484b4acef6538f24da783
Sandatang <mushi.elite@gmail.com>
2023-12-15 15:26:21 +0800

    migration fix

---
+++ backend/CampusCore.API/Migrations/20231215023159_HasRetainableGroup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusCore.API.Migrations
+{
+    public partial class HasRetainableGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasRetainableGroup",
+                table: "Courses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasRetainableGroup",
+                table: "Courses");
+        }
+    }
+}
---
+++ backend/CampusCore.API/Migrations/20231215040254_NullableOfferedCourseId.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampusCore.API.Migrations
+{
+    public partial class NullableOfferedCourseId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_OfferedCourses_OfferedCourseId",
+                table: "Groups");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OfferedCourseId",
+                table: "Groups",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_OfferedCourses_OfferedCourseId",
+                table: "Groups",
+                column: "OfferedCourseId",
+                principalTable: "OfferedCourses",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_OfferedCourses_OfferedCourseId",
+                table: "Groups");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OfferedCourseId",
+                table: "Groups",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_OfferedCourses_OfferedCourseId",
+                table: "Groups",
+                column: "OfferedCourseId",
+                principalTable: "OfferedCourses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
